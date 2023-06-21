0ead84182729ab580c59eeb4f312821053475833
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2023-06-21 09:05:01 -0300

    2015-03

---
+++ 2015/day03/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+type Santa struct {
+	x, y int
+}
+
+func (s *Santa) move(dir string) {
+	if dir == ">" {
+		s.x++
+	}
+	if dir == "<" {
+		s.x--
+	}
+	if dir == "^" {
+		s.y++
+	}
+	if dir == "v" {
+		s.y--
+	}
+	return
+}
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	houses := make(map[string]int)
+	santa := &Santa{0, 0}
+	houses["0,0"]++
+	for _, d := range strings.Split(input, "") {
+		santa.move(d)
+		houses[fmt.Sprintf("%d,%d", santa.x, santa.y)]++
+	}
+	return len(houses)
+}
+
+func part2(input string) int {
+	houses := make(map[string]int)
+	santa := &Santa{0, 0}
+	robot := &Santa{0, 0}
+	houses["0,0"]++
+	for ind, d := range strings.Split(input, "") {
+		if ind%2 == 0 {
+			santa.move(d)
+			houses[fmt.Sprintf("%d,%d", santa.x, santa.y)]++
+		} else {
+			robot.move(d)
+			houses[fmt.Sprintf("%d,%d", robot.x, robot.y)]++
+		}
+	}
+	return len(houses)
+}
