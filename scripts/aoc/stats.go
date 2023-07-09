bfc06c72c68c2c071bf92b2aecf1c8b8586e2471
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2023-07-09 14:27:39 -0300

    Fetch AOCs stats and write to markdown file

--- scripts/aoc/stats.go
+++ scripts/aoc/stats.go
@@ -2,11 +2,38 @@ package aoc
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
-func GetStats(year int, cookie string) (int, map[string]int) {
+func GetStats(cookie string) (int, map[string]int) {
+	years := []int{2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022}
+	allYearsTotal := 0
+	stars := make(map[string]int)
+	for _, year := range years {
+		total, _ := GetStatsForYear(year, cookie)
+
+		stars[fmt.Sprintf("%d", year)] = total
+		allYearsTotal += total
+	}
+
+	table := generateTable(stars, allYearsTotal)
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	// write to file
+	filename := filepath.Join(path, "_stats.md")
+	WriteToFile(filename, []byte(table))
+
+	return allYearsTotal, stars
+}
+
+func GetStatsForYear(year int, cookie string) (int, map[string]int) {
 	days := make(map[string]int)
 	total := 0
 	// make the request
@@ -36,4 +63,18 @@ func GetStats(year int, cookie string) (int, map[string]int) {
 
 	}
 	return total, days
+}
+
+func generateTable(stars map[string]int, total int) string {
+	years := []int{2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022}
+	output := []string{}
+	output = append(output, "| Year | Stars |\n")
+	output = append(output, "| ---- | ----- |\n")
+	for _, year := range years {
+		output = append(output, fmt.Sprintf("| %d | %03d ⭐ |\n", year, stars[fmt.Sprintf("%d", year)]))
+	}
+
+	output = append(output, fmt.Sprintf("| Total | %03d ⭐ |\n", total))
+
+	return strings.Join(output, "")
 }
--- scripts/cmd/stats/main.go
+++ scripts/cmd/stats/main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/krewenki/advent-of-code/scripts/aoc"
 )
 
 func main() {
 	_, _, cookie := aoc.ParseFlags()
-	years := []int{2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022}
-	allYearsTotal := 0
-	stars := make(map[string]int)
-	output := []string{}
-	output = append(output, "| Year | Stars |\n")
-	output = append(output, "| ---- | ----- |\n")
-	for _, year := range years {
-		total, _ := aoc.GetStats(year, cookie)
 
-		stars[fmt.Sprintf("%d", year)] = total
-		allYearsTotal += total
-
-		// sleep for 1 second
-		time.Sleep(time.Second)
-	}
-
-	for _, year := range years {
-		output = append(output, fmt.Sprintf("| %d | %03d ⭐ |\n", year, stars[fmt.Sprintf("%d", year)]))
-	}
-
-	output = append(output, fmt.Sprintf("| Total | %03d ⭐ |\n", allYearsTotal))
-
+	aoc.GetStats(cookie)
 }
