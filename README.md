e38f75a1f6682d5a9c2eae1ddd22774e4c5ad66b
Warren Krewenki <19960+krewenki@users.noreply.github.com>
2023-07-09 14:10:24 -0300

    Fetch stats about stars from aoc

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Advent Of Code
 Go solutions for advent of code problems, mainly for discussing solutions on private leaderboards.
 
 Hat tip to [alexchao26](https://github.com/krewenki/advent-of-code) for posting his skeleton script online, it saved me a lot of copy and pasting to get each year laid out.
+
+
+Stats
+---
--- scripts/aoc/stats.go
+++ scripts/aoc/stats.go
@@ -2,18 +2,38 @@ package aoc
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
-func GetStats(year int, cookie string) {
-	fmt.Printf("fetching for year %d\n", year)
-
+func GetStats(year int, cookie string) (int, map[string]int) {
+	days := make(map[string]int)
+	total := 0
 	// make the request
-	url := fmt.Sprintf("https://adventofcode.com/%d/leaderboard/self", year)
+	url := fmt.Sprintf("https://adventofcode.com/%d", year)
 	body := GetWithAOCCookie(url, cookie)
 
+	r := regexp.MustCompile(`<a aria-label="Day ([0-9]*)[, ]*([a-z ]*)`)
+
 	for _, l := range strings.Split(string(body), "\n") {
-		fmt.Println(l)
+
+		matches := r.FindStringSubmatch(l)
+		if len(matches) == 3 {
+			date := fmt.Sprintf("'%02s'", matches[1])
+			if matches[2] == "two stars" {
+				days[date] = 2
+				total += 2
+			} else if matches[2] == "one star" {
+				days[date] = 1
+				total += 1
+			} else {
+				days[date] = 0
+			}
+
+		} else {
+			continue
+		}
 
 	}
+	return total, days
 }
